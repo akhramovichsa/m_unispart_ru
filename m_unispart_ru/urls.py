4dd7bc00862f5e41483e0737b119f4ec191d9889
Alexey Pavlov <IrishGreenHedgehog@gmail.com>
2015-10-08 16:51:33 +0300

    Повтор

--- m_unispart_ru/urls.py
+++ m_unispart_ru/urls.py
@@ -8,6 +8,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'm_unispart_ru.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^else/$',    TemplateView.as_view(template_name="else.html"), name='else'),
+    url(r'^about/$',    TemplateView.as_view(template_name="about.html"), name='about'),
     url(r'^$',          TemplateView.as_view(template_name="index.html"), name='index'),
     url(r'^contacts/$', TemplateView.as_view(template_name="contacts.html"), name='contacts'),
     url(r'^search/$',   views.search, name='search'),
